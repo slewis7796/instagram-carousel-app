70e9e021612b178bf2c8f1f4dfb40c3e0120031b
Temporary User <temp@example.com>
2025-03-31 07:50:27 -0400

    Remove basePath configuration to fix deployment issues

--- next.config.js
+++ next.config.js
@@ -1,21 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/text-to-insta',
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['*'],
     unoptimized: true
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/text-to-insta',
-        permanent: false,
-      },
-    ]
-  },
+  }
 };
 
 module.exports = nextConfig;
